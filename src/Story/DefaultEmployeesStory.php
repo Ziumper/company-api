c7bcd7d3f367c5d76be69ff3c303fa09798f2bcc
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-03-06 11:31:48 +0100

    add basic test fixture

---
+++ src/Story/DefaultEmployeesStory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Story;
+
+use App\Factory\CompanyFactory;
+use App\Factory\EmployeeFactory;
+use Zenstruck\Foundry\Story;
+
+final class DefaultEmployeesStory extends Story
+{
+    public function build(): void
+    {
+        CompanyFactory::createMany(50);
+        
+        EmployeeFactory::createMany(150,function() {
+            return [
+                "company" => CompanyFactory::random()
+            ];
+        });
+    }
+}
