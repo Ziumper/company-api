e1431ddc34697e34ff3d03824075619f48444a0b
Ziumper <ziumper@gmail.com>
2025-10-04 10:23:08 +0200

    add base api controller

---
+++ src/Controller/Api/BaseApiController.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Entity\BaseEntity;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+abstract readonly class BaseApiController 
+{
+    protected readonly EntityRepository $repository;
+
+    public function __construct(
+        protected SerializerInterface $serializer,
+        protected ValidatorInterface $validator,
+        protected EntityManagerInterface $entityManager,
+    )
+    {
+        $this->repository = $this->entityManager->getRepository($this->getEntityClass());
+    }
+    
+    abstract protected function getEntityClass(): string;
+  
+    public function remove(BaseEntity $entity): JsonResponse
+    {
+        $this->entityManager->remove($entity);
+        $this->entityManager->flush();
+
+        return new JsonResponse(null, 204);
+    }
+
+
+    public function show(BaseEntity $entity): JsonResponse
+    {
+        return new JsonResponse(
+            data: $this->serialize($entity),
+            status: 200,
+            json: true,
+        );
+    }
+    
+    public function list(Request $request): JsonResponse
+    {
+        $page = max(1, (int)$request->query->get('page', 1));
+        $limit = min(50, (int)$request->query->get('limit', 10));
+
+        $offset = ($page - 1) * $limit;
+
+        $companies = $this->repository->findBy([], null, $limit, $offset);
+
+        $data = [
+            'page' => $page,
+            'limit' => $limit,
+            'total' => $this->repository->count(),
+            'data' => $companies,
+        ];
+
+        return new JsonResponse($this->serialize($data), 200, ['content-type' => 'application/json'], true);
+    }
+    
+    public function add(
+        Request $request,
+    ): JsonResponse {
+        $data = $request->getContent();
+        
+        $entity = $this->deserialize($data);
+        $errors = $this->validator->validate($entity);
+        
+        if (count($errors) > 0) {
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), 400, [], true);
+        }
+        
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+               
+        return $this->show($entity);
+    }
+    
+    protected function serialize(mixed $data, array $groups = ['read']): string 
+    {
+        return $this->serializer->serialize($data, 'json', [
+            'groups' => $groups,
+            'json_encode_options' => JSON_UNESCAPED_UNICODE,
+        ]);
+    }
+    
+    protected function deserialize(string $data, array $groups = [ 'create'] ): mixed
+    {
+        $config = [
+            'groups' => $groups,
+        ];
+        
+        return  $this->serializer->deserialize($data, $this->getEntityClass(), 'json', $config);
+    }
+        
+}
--- src/Controller/Api/CompanyController.php
+++ src/Controller/Api/CompanyController.php
@@ -6,115 +6,62 @@ namespace App\Controller\Api;
 
 use App\Entity\Company;
 use App\Entity\Employee;
-use App\Repository\CompanyRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Override;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/company')]
-class CompanyController
-{
-    #[Route('', methods:['GET'])]
-    public function index(Request $request, CompanyRepository $companyRepository, SerializerInterface $serializer): JsonResponse
+readonly class CompanyController extends BaseApiController
+{   
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(Company $company): JsonResponse
     {
-        $page = max(1, (int)$request->query->get('page', 1));
-        $limit = min(50, (int)$request->query->get('limit', 10));
-
-        $offset = ($page - 1) * $limit;
-
-        $companies = $companyRepository->findBy([], null, $limit, $offset);
-
-        $data = [
-            'page' => $page,
-            'limit' => $limit,
-            'total' => $companyRepository->count(),
-            'data' => $companies,
-        ];
-        
-        $json = $serializer->serialize($data, 'json', [
-            'groups' => ['read'],
-            'json_encode_options' => JSON_UNESCAPED_UNICODE,
-        ]);
-
-        return new JsonResponse($json, 200, ['content-type' => 'application/json'], true);
+       return $this->remove($company);
+    }
+    
+    #[Route('/{id}', methods: ['GET'])]
+    public function get(Company $company): JsonResponse 
+    {
+        return $this->show($company);
+    }
+    
+    #[Route('', methods:['GET'])]
+    public function index(Request $request): JsonResponse  
+    {
+        return $this->list($request);
     }
     
     #[Route('', methods: ['POST'])]
-    public function create(
-        Request $request,
-        SerializerInterface $serializer,
-        ValidatorInterface $validator,
-        EntityManagerInterface $em
-    ): JsonResponse {
-        $data = $request->getContent();
-        
-        $company = $serializer->deserialize($data, 
-                Company::class, 
-                'json',
-                ['groups' => ['create']]);
-        
-        $errors = $validator->validate(value: $company);
-        
-        if (count($errors) > 0) {
-            return new JsonResponse($serializer->serialize($errors, 'json'), 400, [], true);
-        }
-        
-        $em->persist($company);
-        $em->flush();
-                
-        $mydata = $serializer->serialize($company, 'json', 
-        [
-            'groups' => ['read'],
-            'json_encode_options' => JSON_UNESCAPED_UNICODE,
-        ]);
-        
-        return new JsonResponse($mydata, 201, [], true);
-    }
-
-    #[Route('/{id}', methods: ['GET'])]
-    public function show(Company $company, SerializerInterface $serializer): JsonResponse
+    public function create(Request $request): JsonResponse 
     {
-        return new JsonResponse(
-            $json = $serializer->serialize($company, 'json', [
-            'groups' => ['read'],
-            'json_encode_options' => JSON_UNESCAPED_UNICODE,
-             ]),
-            200,
-            [],
-            true
-        );
+       return $this->add($request);
     }
-
+    
     #[Route('/{id}', methods: ['PUT'])]
     public function update(
         Request $request,
         Company $company,
-        SerializerInterface $serializer,
-        ValidatorInterface $validator,
-        EntityManagerInterface $em,
     ): JsonResponse {
-        
-        $serializer->deserialize($request->getContent(), Company::class, 'json', [
+         
+        $this->serializer->deserialize($request->getContent(), $this->getEntityClass(), 'json', [
             'object_to_populate' => $company,
             'groups' => ['update'],
         ]);
         
-        $errors = $validator->validate($company);
+        $errors = $this->validator->validate($company);
         if (count($errors) > 0) {
-            return new JsonResponse($serializer->serialize($errors, 'json'), 400, [], true);
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), 400, [], true);
         }
 
-         if ($company->getEmployeers()->count() > 0) {
+        if ($company->getEmployeers()->count() > 0) {
             $ids = [];
             
             foreach($company->getEmployeers() as $employeer) {
                 $ids[] = $employeer->getId();
             }
           
-            $repository = $em->getRepository(Employee::class);
+            $repository = $this->entityManager->getRepository(Employee::class);
             $employeers = $repository->findBy(['id' => $ids]);
             
             foreach($employeers as $employeer) {
@@ -124,22 +71,13 @@ class CompanyController
             }        
         }
         
-        $em->flush();
+        $this->entityManager->flush();
 
-        $json = $serializer->serialize($company, 'json', [
-            'groups' => ['read'],
-            'json_encode_options' => JSON_UNESCAPED_UNICODE,
-        ]);
-        
-        return new JsonResponse($json, 200, [], true);
+        return new JsonResponse($this->serialize($company), 200, [], true);
     }
-
-    #[Route('/{id}', methods: ['DELETE'])]
-    public function delete(Company $company, EntityManagerInterface $em): JsonResponse
-    {
-        $em->remove($company);
-        $em->flush();
-
-        return new JsonResponse(null, 204);
+    
+    #[Override]
+    protected function getEntityClass(): string {
+        return Company::class;
     }
 }
